0179451bb5144a4edb83005dc5a04e0f957e9843
ankit <ankit.chaturvedi@manprax.com>
2019-05-30 13:18:12 +0530

    Added date format.

--- app/src/main/java/com/mx/demoapplication/utils/Constants.kt
+++ app/src/main/java/com/mx/demoapplication/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.mx.demoapplication.utils
 class Constants {
 
     companion object{
-        val api_key = "827f944f48444712b8e22b473c4626cb"
+        val api_key = "api key"
         val countryCode = "in"
     }
 }
